d83206a00677ad7e9e0a2b6f9cb3904815a629ef
Anang Adi <anang.adi182@gmail.com>
2024-08-09 19:50:03 +0700

    add campagin entity description

--- campaign/entity.go
+++ campaign/entity.go
@@ -7,6 +7,7 @@ type Campaign struct {
 	UserID           int
 	Name             string
 	ShortDescription string
+	Description      string
 	Perks            string
 	BackerCount      int
 	GoalAmount       int
